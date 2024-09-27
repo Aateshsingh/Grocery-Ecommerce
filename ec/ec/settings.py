8d6f2094f3fbf587f41996915dca85b6fb776a8a
Aateshsingh <113357055+Aateshsingh@users.noreply.github.com>
2024-09-27 16:34:43 +0530

    Few Updates

--- ec/ec/settings.py
+++ ec/ec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0d_2c92d1l&#3bu)$aa!y#24k5fff+^2@&g)&mh)=@h0=e6^vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
